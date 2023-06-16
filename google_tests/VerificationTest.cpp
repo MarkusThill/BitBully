fd49e3294de4a15f4cb419a8251c5b969cffb6cf
m.thill <m.thill@precitec.de>
2023-06-17 00:47:23 +0200

    renamed a few variables to english names

--- google_tests/VerificationTest.cpp
+++ google_tests/VerificationTest.cpp
@@ -17,16 +17,16 @@ class ExampleTest : public ::testing::Test {
 protected:
 
     void SetUp() override {
-        Reset(); //entleert das Spielfeld. Die Variablen Feld1 und Feld2 werden auf 0 gesetzt.
-        BuchLaden("../../book.dat");  //Die Er�ffnungsdatenbank wird direkt zu beginn aus der Datei eingelesen
-        BuchLaden2("../../data8ply.dat");
-        MaxInstance=100;  //Der Standardwert f�r die Schwierigkeitsstufe wird gesetzt. Die Schwierigkeitsstufe ergibt sich aus (MaxInstance-2)/2
-        Modus=0;         //Da noch kein Spielmodus ausgew�hlt wurde
-        HASHE = (HASHEvorlage *)malloc(HASHSIZE * sizeof(HASHEvorlage));
-        LHASHE = (HASHEvorlage *)malloc(LHASHSIZE * sizeof(HASHEvorlage));
-        DRHASHE = (DRVorlage *)malloc(DRHASHSIZE * sizeof(DRVorlage));
-        ResetHash();
-        ModusEinrichten();
+        // TODO: We need a constructor
+        c4.Reset(); //entleert das Spielfeld. Die Variablen m_fieldP1 und m_fieldP2 werden auf 0 gesetzt.
+        c4.BuchLaden("../../book.dat");  //Die Er�ffnungsdatenbank wird direkt zu beginn aus der Datei eingelesen
+        c4.BuchLaden2("../../data8ply.dat");
+        c4.setMaxInstance(
+                100);  //Der Standardwert fÜr die Schwierigkeitsstufe wird gesetzt. Die Schwierigkeitsstufe ergibt sich aus (m_maxSearchDepth-2)/2
+        c4.Modus = 0;         //Da noch kein Spielmodus ausgew�hlt wurde
+        c4.initHash();
+        c4.ResetHash();
+        c4.ModusEinrichten();
 
         std::cout << "Version: v" << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << std::endl;
     }
@@ -37,28 +37,30 @@ protected:
     ~ExampleTest() override {
         //resources cleanup, no exceptions allowed
     }
+
+    Connect4 c4;
 };
 
 TEST_F(ExampleTest, fastVerificationTest) {
     int x, i;
     time_point time_start = std::chrono::high_resolution_clock::now();
-    for(i=0;i<86892;i+=100)
-    {
-        Feld1=OPENING[i].FeldData1;
-        Feld2=OPENING[i].FeldData2;
-        ResetHash();
-        HoeheErmitteln();
-        InstanceVoll=42-BrettCount();
-        MaxInstance=100;
-        x = WurzelMethodeComputerAnziehender(0, -9999, 9999, HoeheErmitteln());
-        EXPECT_FALSE(x==1000 && OPENING[i].Win!=2);
-        EXPECT_FALSE(x==-1000 && OPENING[i].Win!=1);
-        EXPECT_FALSE(x==0 && OPENING[i].Win!=0);
-        EXPECT_FALSE(x!= -1000 && x!= 1000 && x!=0);
+    for (i = 0; i < 86892; i += 100) {
+        auto entry = c4.getOpening(i);
+        c4.setFeld(entry.m_positionP1, entry.m_positionP2);
+        c4.ResetHash();
+        c4.HoeheErmitteln();
+
+        c4.setDepthTie();
+        c4.setMaxInstance(100);
+        x = c4.WurzelMethodeComputerAnziehender(0, -9999, 9999, c4.HoeheErmitteln());
+        EXPECT_FALSE(x == 1000 && entry.m_value != 2);
+        EXPECT_FALSE(x == -1000 && entry.m_value != 1);
+        EXPECT_FALSE(x == 0 && entry.m_value != 0);
+        EXPECT_FALSE(x != -1000 && x != 1000 && x != 0);
         std::cout << "Done with " << i << std::endl;
     }
     time_point time_end = std::chrono::high_resolution_clock::now();
 
     float dur = float(duration(time_end - time_start).count());
-    std::cout << "Total Time for fastVerificationTest(): " << dur << "seconds" << std::endl;
+    std::cout << "Total Time for fastVerificationTest(): " << dur << " seconds" << std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -9,23 +9,23 @@ void verify()
     int x, i;
     for(i=0;i<86892;i+=7) //40
     {
-        Feld1=OPENING[i].FeldData1;
-        Feld2=OPENING[i].FeldData2;
+        Feld1=OPENING[i].m_positionP1;
+        Feld2=OPENING[i].m_positionP2;
         ResetHash();
         HoeheErmitteln();
         InstanceVoll=42-BrettCount();
         MaxInstance=100;
         x = WurzelMethodeComputerAnziehender(0, -9999, 9999, HoeheErmitteln());
-        if(x==1000 && OPENING[i].Win!=2) {
-            std::cerr << "i=" << i << ", x=" << x  << ":  if(x==1000 && OPENING[i].Win!=2)" << std::endl;
+        if(x==1000 && OPENING[i].m_value != 2) {
+            std::cerr << "i=" << i << ", x=" << x  << ":  if(x==1000 && m_pOpeningBook8Ply_2[i].m_value!=2)" << std::endl;
             break;
         }
-        else if(x==-1000 && OPENING[i].Win!=1) {
-            std::cerr << "i=" << i << ", x=" << x  << ": else if(x==-1000 && OPENING[i].Win!=1)" << std::endl;
+        else if(x==-1000 && OPENING[i].m_value != 1) {
+            std::cerr << "i=" << i << ", x=" << x  << ": else if(x==-1000 && m_pOpeningBook8Ply_2[i].m_value!=1)" << std::endl;
             break;
         }
-        else if(x==0 && OPENING[i].Win!=0) {
-            std::cerr << "i=" << i << ", x=" << x  << ": else if(x==0 && OPENING[i].Win!=0)" << std::endl;
+        else if(x==0 && OPENING[i].m_value != 0) {
+            std::cerr << "i=" << i << ", x=" << x  << ": else if(x==0 && m_pOpeningBook8Ply_2[i].m_value!=0)" << std::endl;
             break;
         }
         else if(x!= -1000 && x!= 1000 && x!=0) {
@@ -37,10 +37,10 @@ void verify()
 }
 
 int main() {
-    Reset(); //entleert das Spielfeld. Die Variablen Feld1 und Feld2 werden auf 0 gesetzt.
-    BuchLaden("../book.dat");  //Die Er�ffnungsdatenbank wird direkt zu beginn aus der Datei eingelesen
+    Reset(); //entleert das Spielfeld. Die Variablen m_fieldP1 und m_fieldP2 werden auf 0 gesetzt.
+    BuchLaden("../m_openingBook8Ply.dat");  //Die Er�ffnungsdatenbank wird direkt zu beginn aus der Datei eingelesen
     BuchLaden2("../data8ply.dat");
-    MaxInstance=100;  //Der Standardwert f�r die Schwierigkeitsstufe wird gesetzt. Die Schwierigkeitsstufe ergibt sich aus (MaxInstance-2)/2
+    MaxInstance=100;  //Der Standardwert f�r die Schwierigkeitsstufe wird gesetzt. Die Schwierigkeitsstufe ergibt sich aus (m_maxSearchDepth-2)/2
     Modus=0;         //Da noch kein Spielmodus ausgew�hlt wurde
     HASHE = (HASHEvorlage *)malloc(HASHSIZE * sizeof(HASHEvorlage));
     LHASHE = (HASHEvorlage *)malloc(LHASHSIZE * sizeof(HASHEvorlage));
