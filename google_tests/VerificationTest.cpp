47b0568c5955da500b17fb5cda554d508a1c405c
m.thill <m.thill@precitec.de>
2023-06-20 06:51:00 +0200

    added Martin Fierz's C4 agent for verification purposes.

--- google_tests/VerificationTest.cpp
+++ google_tests/VerificationTest.cpp
@@ -7,6 +7,7 @@
 #include <chrono>
 #include "gtest/gtest.h"
 #include "cpart.cpp"
+#include "4inarow.h"
 
 using time_point = std::chrono::time_point<std::chrono::high_resolution_clock>;
 using duration = std::chrono::duration<float>;
@@ -89,4 +90,75 @@ TEST_F(ExampleTest, fastVerificationTest) {;
 
     float dur = float(duration(time_end - time_start).count());
     std::cout << "Total Time for fastVerificationTest(): " << dur << " seconds" << std::endl;
+}
+
+
+static int level=86000000;//  =10; //level set to intermediate as default.
+static int originalboard [7][6];
+
+// settings passed to engine
+int usethebook = 1;
+int randomize = 0;
+int searchwin = 0;
+int gametheoretic = 0;
+int evaltype = EVAL_SMART;
+
+enum levels {BEGINNER,INTERMEDIATE,EXPERT,MASTER,INFLEVEL,GAMETHEORETIC,SEARCHWIN} levell;
+int leveltime[7] = {0,8,30,300,8600000,86000000,86000000};
+
+void setlevel(int l)
+{
+    level=l;
+}
+
+int getcomputermove(int color, int *play)
+{
+    char out[255];
+    int bestmove;
+
+    double maxtime;
+    maxtime=((double)level)/10.0;
+
+    sprintf(out,"calling findmove");
+    bestmove = findmove(originalboard,color,maxtime,out, play,searchwin, randomize, usethebook, gametheoretic, evaltype );
+    std::cout << out << std::endl;
+
+    return(bestmove);
+}
+
+TEST_F(ExampleTest, doenerTest) {
+    int color = 1; // 1== YELLOW, -1 ==RED???
+
+    /*
+    levell = INFLEVEL;
+    gametheoretic = 0;
+    searchwin = 0;
+    setlevel(leveltime[levell]);
+     */
+    levell = GAMETHEORETIC;
+    gametheoretic=1;
+    searchwin=0;
+    setlevel(leveltime[GAMETHEORETIC]);
+
+    originalboard [3][0] = 1;
+    originalboard [3][1] = -1;
+    originalboard [3][2] = 1;
+    originalboard [3][3] = -1;
+    originalboard [3][4] = 1;
+    originalboard [1][0] = -1;
+    originalboard [1][1] = 1;
+    originalboard [1][2] = -1;
+
+    int playnow = 0;
+    auto bestmove = getcomputermove(color, &playnow);
+    std::cout << "best move:" << bestmove << std::endl;
+
+    std::cout << "playnow:" << playnow << std::endl;
+
+    extern int64 hashhits, hashmisses;
+    std::cout << "hashhits: " << hashhits << ", hashmisses: " << hashmisses << std::endl;
+
+
+    bestmove = getcomputermove(color, &playnow);
+    std::cout << std::endl << "hashhits: " << hashhits << ", hashmisses: " << hashmisses << std::endl;
 }
