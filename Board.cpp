be4f9a399aca818a2600314d5eb2053f7e3f9e2c
MarkusThill <8896197+MarkusThill@users.noreply.github.com>
2023-09-02 23:49:26 +0200

    Implemented a hasWin function using a new bitboard layout.

--- Board.cpp
+++ Board.cpp
@@ -1,68 +1,49 @@
 #include "Board.h"
 #include <array>
 #include <cassert>
+#include <iostream>
 
 namespace BitBully {
 
-Board::Board() : {}
+Board::Board()
+    : m_bActive{BB_EMPTY}, //
+      m_bAll{BB_EMPTY},    //
+      m_movesLeft{N_COLUMNS * N_ROWS} {
+  // TODO: where to put some general asserts???
+  // Some asserts that will be turned off in Release mode
+  assert(uint64_t_popcnt(BB_ALL_LEGAL_TOKENS) == N_COLUMNS * N_ROWS);
+  assert(uint64_t_popcnt(BB_ILLEGAL) ==
+         CHAR_BIT * sizeof(TBitBoard) - N_COLUMNS * N_ROWS);
+}
 
 bool Board::setBoard(const TBoardArray &board) {
   if (!isValid(board)) {
     return false;
   }
-
-  TBitBoard bbYellow{UINT64_C(0)}, bbRed{UINT64_C(0)};
+  TBitBoard allTokens{BB_EMPTY}, yellowTokens{BB_EMPTY};
   for (auto c = 0; c < N_COLUMNS; ++c) {
     for (auto r = 0; r < N_ROWS; ++r) {
       auto val = board[c][r];
-      auto mask = getMask(c, r);
+      auto mask = getMaskColRow(c, r);
       if (val == P_YELLOW) {
-        bbYellow ^= mask;
-      } else if (val == P_RED) {
-        bbRed ^= mask;
-      } else {
+        yellowTokens ^= mask;
+      } else if (val != P_RED) {
         assert(val == P_EMPTY);
+        continue;
       }
+      assert((allTokens & mask) == BB_EMPTY);
+      allTokens ^= mask;
     }
   }
 
-  // TODO:
-  // ... set private members
+  m_movesLeft =
+      N_COLUMNS * N_ROWS - static_cast<int>(uint64_t_popcnt(allTokens));
+
+  // if m_movesLeft is odd that means that its players RED turn
+  m_bActive = yellowTokens ^ (m_movesLeft & 1 ? allTokens : BB_EMPTY);
+  m_bAll = allTokens;
+
   return true;
-}
-
-Board::THeightsArray Board::getColumnHeights(const Board::TBitBoard &bbYellow,
-                                             const Board::TBitBoard &bbRed) {
-  THeightsArray heights;
-  const auto bbYellowRed = bbYellow | bbRed;
-  for (auto i = 0; i < N_COLUMNS; i++) {
-    THeight j;
-    for (j = 0; j < N_ROWS; j++) {
-      auto mask = getMask(i, j);
-      if ((bbYellowRed & mask) != mask)
-        break;
-    }
-    heights[i] = j;
-  }
-  return heights;
-}
-
-Board::TBoardArray Board::toArray(const Board::TBitBoard &bbYellow,
-                                  const Board::TBitBoard &bbRed) {
-  TBoardArray board;
-  for (int r = N_ROWS - 1; r >= 0; r--) {
-    for (int c = 0; c < N_COLUMNS; c++) {
-      auto mask = getMask(c, r);
-      if (bbRed & mask) {
-        board[c][r] = P_RED;
-      } else if (bbYellow & mask) {
-        board[c][r] = P_YELLOW;
-      } else {
-        board[c][r] = P_EMPTY;
-      }
-    }
-  }
-  return board;
 }
 
 bool Board::isValid(const Board::TBoardArray &board) {
@@ -95,20 +76,168 @@ bool Board::isValid(const Board::TBoardArray &board) {
   return true;
 }
 
-void Board::playMoveFast(int column) {
-  // TODO:...
-}
-
+/* [ *,  *,  *,  *,  *,  *,  *]
+ * [ *,  *,  *,  *,  *,  *,  *]
+ * [ *,  *,  *,  *,  *,  *,  *]
+ * [59, 50, 41, 32, 23, 14,  5],
+ * [58, 49, 40, 31, 22, 13,  4],
+ * [57, 48, 39, 30, 21, 12,  3],
+ * [56, 47, 38, 29, 20, 11,  2],
+ * [55, 46, 37, 28, 19, 10,  1],
+ * [54, 45, 36, 27, 18,  9,  0]
+ */
 bool Board::playMove(int column) {
-  if (column < 0 || column >= N_COLUMNS)
+  // Check, if column full...
+  if (!isLegalMove(column))
     return false;
 
-  // Check, if column full...
-  // get column bit mask
-  // which board to set? bYellow or red?
+  playMoveFast(column);
+
   return true;
 }
 
-bool Board::hasWin() { return false; }
+bool Board::isLegalMove(int column) {
+  if (column < 0 || column >= N_COLUMNS)
+    return false;
+
+  TBitBoard columnMask = getColumnMask(column);
+  columnMask -= (UINT64_C(1) << (column * COLUMN_BIT_OFFSET));
+  return !(m_bAll & columnMask & BB_TOP_ROW);
+}
+
+/*Board::TBitBoard Board::winningPositions(TBitBoard x) {
+  // Vertical wins are fairly simple:
+  TBitBoard wins = (x << 1) & (x << 2) & (x << 3);
+
+  // ====================================================================
+  // Horizontal Wins (move tokens by +-9 bits)
+  // tokens shifted by 1 & 2 columns to the left (this avoids some redundant
+  // computations below):
+  auto tmp = (x << COLUMN_BIT_OFFSET) & (x << 2 * COLUMN_BIT_OFFSET);
+  wins |= tmp & (x << 3 * COLUMN_BIT_OFFSET); // A = bcd
+  wins |= tmp & (x >> COLUMN_BIT_OFFSET);     // B = bcd
+
+  // tokens shifted by 1 & 2 columns to the right:
+  tmp = (x >> COLUMN_BIT_OFFSET) & (x >> 2 * COLUMN_BIT_OFFSET);
+  wins |= tmp & (x << COLUMN_BIT_OFFSET);     // C = abd
+  wins |= tmp & (x >> 3 * COLUMN_BIT_OFFSET); // D = abc
+  // ====================================================================
+
+  // ====================================================================
+  // Diagonal Wins 1: bottom-left to top-right (move tokens by +-8 bits)
+  // tokens shifted 1 & 2 positions towards the bottom-left:
+  constexpr auto DIAG_OFFSET1 = COLUMN_BIT_OFFSET - 1;
+  tmp = (x << DIAG_OFFSET1) & (x << 2 * DIAG_OFFSET1);
+  wins |= tmp & (x << 3 * DIAG_OFFSET1); // A = bcd
+  wins |= tmp & (x >> DIAG_OFFSET1);     // B = bcd
+
+  // tokens shifted by 1 & 2 positions towards the top-right:
+  tmp = (x >> DIAG_OFFSET1) & (x >> 2 * DIAG_OFFSET1);
+  wins |= tmp & (x << DIAG_OFFSET1);     // C = abd
+  wins |= tmp & (x >> 3 * DIAG_OFFSET1); // D = abc
+
+  // TODO: Not checked..
+  // ====================================================================
+  // Diagonal Wins 2: bottom-left to top-right (move tokens by +-8 bits)
+  // tokens shifted 1 & 2 positions towards the top-left:
+  constexpr auto DIAG_OFFSET2 = COLUMN_BIT_OFFSET + 1;
+  tmp = (x << DIAG_OFFSET2) & (x << 2 * DIAG_OFFSET2);
+  wins |= tmp & (x << 3 * DIAG_OFFSET2); // A = bcd
+  wins |= tmp & (x >> DIAG_OFFSET2);     // B = bcd
+
+  // tokens shifted by 1 & 2 positions towards the bottom-right:
+  tmp = (x >> DIAG_OFFSET2) & (x >> 2 * DIAG_OFFSET2);
+  wins |= tmp & (x << DIAG_OFFSET2);     // C = abd
+  wins |= tmp & (x >> 3 * DIAG_OFFSET2); // D = abc
+
+  return wins & BB_ALL_LEGAL_TOKENS;
+}
+ */
+
+/*
+ * Simplified case: Position x is a nibble (4 bit): x = abcd
+ *
+ * a b c d | A B C D
+ * --------+--------
+ * 0 0 0 0 | 0 0 0 0
+ * 0 0 0 1 | 0 0 0 0
+ * 0 0 1 0 | 0 0 0 0
+ * 0 0 1 1 | 0 0 0 0
+ * 0 1 0 0 | 0 0 0 0
+ * 0 1 0 1 | 0 0 0 0
+ * 0 1 1 0 | 0 0 0 0
+ * 0 1 1 1 | 0 0 0 0  < A = bcd
+ * 1 0 0 0 | 0 0 0 0
+ * 1 0 0 1 | 0 0 0 0
+ * 1 0 1 0 | 0 0 0 0
+ * 1 0 1 1 | 0 0 0 0  < B = acd
+ * 1 1 0 0 | 0 0 0 0
+ * 1 1 0 1 | 0 0 1 0  < C = abd
+ * 1 1 1 0 | 0 0 0 1  < D = abc
+ * 1 1 1 1 | * * * *  < Should not happen (since it already contains a win)
+ *
+ * In order to check if C = abd is true (putting a token in "c" would win), we
+ * have to shift the remaining bits "a", "b", "d" onto the position "c" and then
+ * perform a bitwise "AND". In the simplified tabular case above, we could do
+ * something like " C = (x >> 2) & (x >> 1) & ( x << 1)
+ * Per direction (vertical is an exception) we have 4 possible winning positions
+ * A, B, C and, D. We have to check all 4 of them. However, in order to win, the
+ * target position has to be empty and reachable. This has to be checked
+ * subsequently.
+ * TODO: inspired by John Tromp and Pascal Pons
+ */
+/* [ *,  *,  *,  *,  *,  *,  *]
+ * [ *,  *,  *,  *,  *,  *,  *]
+ * [ *,  *,  *,  *,  *,  *,  *]
+ * [59, 50, 41, 32, 23, 14,  5],
+ * [58, 49, 40, 31, 22, 13,  4],
+ * [57, 48, 39, 30, 21, 12,  3],
+ * [56, 47, 38, 29, 20, 11,  2],
+ * [55, 46, 37, 28, 19, 10,  1],
+ * [54, 45, 36, 27, 18,  9,  0]
+ */
+Board::TBitBoard Board::winningPositions(TBitBoard x) {
+  // Vertical wins are fairly simple:
+  TBitBoard wins = (x << 1) & (x << 2) & (x << 3);
+
+  for (auto bOff = COLUMN_BIT_OFFSET - 1; bOff <= COLUMN_BIT_OFFSET + 1;
+       ++bOff) {
+    // tokens shifted by 1 & 2 positions left, up-left and down-left (this
+    // avoids some redundant computations below):
+    auto tmp = (x << bOff) & (x << 2 * bOff);
+    wins |= tmp & (x << 3 * bOff); // A = bcd
+    wins |= tmp & (x >> bOff);     // B = bcd
+
+    // tokens shifted by 1 & 2 positions right, up-right and down-right:
+    tmp = (x >> bOff) & (x >> 2 * bOff);
+    wins |= tmp & (x << bOff);     // C = abd
+    wins |= tmp & (x >> 3 * bOff); // D = abc
+  }
+
+  return wins & BB_ALL_LEGAL_TOKENS;
+}
+
+bool Board::hasWin() {
+  return winningPositions(m_bActive) & (m_bAll + BB_BOTTOM_ROW);
+}
+
+Board::TBoardArray Board::toArray() {
+  TBoardArray board{0};
+  auto activePlayer = (m_movesLeft & 1 ? P_RED : P_YELLOW);
+  auto inactivePlayer = opponent(activePlayer);
+  for (auto c = 0; c < N_COLUMNS; ++c) {
+    for (auto r = 0; r < N_ROWS; ++r) {
+      auto mask = getMaskColRow(c, r);
+      if (m_bActive & mask) {
+        board[c][r] = activePlayer;
+      } else if (m_bAll & mask) {
+        board[c][r] = inactivePlayer;
+      } else {
+        board[c][r] = P_EMPTY;
+      }
+    }
+  }
+  return board;
+}
 
 } // namespace BitBully
--- Board.h
+++ Board.h
@@ -2,10 +2,60 @@
 #define XBITBULLY__BOARD_H_
 
 #include <array>
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
+#include <sstream>
+
+// TODO: Measure the time differences. Is it really worth the hassle here???
+/*
+ * // https://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetParallel
+ * A generalization of the best bit counting method to integers of bit-widths
+upto 128 (parameterized by type T) is this:
+
+v = v - ((v >> 1) & (T)~(T)0/3);                           // temp
+v = (v & (T)~(T)0/15*3) + ((v >> 2) & (T)~(T)0/15*3);      // temp
+v = (v + (v >> 4)) & (T)~(T)0/255*15;                      // temp
+c = (T)(v * ((T)~(T)0/255)) >> (sizeof(T) - 1) * CHAR_BIT; // count
+*/
+#if __GNUC__
+#define uint64_t_popcnt __builtin_popcountll
+#else
+#if _MSC_VER
+#include <intrin.h>
+#define uint64_t_popcnt __popcnt64
+#else
+#define uint64_t_popcnt popCountBoard
+#endif
+#endif
 
 namespace BitBully {
+
+constexpr int CHAR_BIT = 8;
+
+static constexpr uint64_t getMask(std::initializer_list<int> bits) {
+  uint64_t bb{UINT64_C(0)};
+  for (const auto i : bits) {
+    bb |= (UINT64_C(1) << i);
+  }
+  return bb;
+}
+
+static constexpr bool isIllegalBit(int bitIdx) {
+  constexpr int COLUMN_BIT_OFFSET = 9; // TODO: redundant in class below. Fix??
+  constexpr int N_ROWS = 6;            // TODO: redundant in class below. Fix??
+  constexpr int COLUMNS = 7;           // TODO: redundant in class below. Fix??
+  return bitIdx >= COLUMN_BIT_OFFSET * COLUMNS ||
+         (bitIdx % COLUMN_BIT_OFFSET) / N_ROWS;
+}
+
+static constexpr uint64_t illegalBitMask() {
+  uint64_t bb{UINT64_C(0)};
+  for (auto i = 0; i < CHAR_BIT * sizeof(uint64_t); ++i) {
+    bb ^= (isIllegalBit(i) ? UINT64_C(1) << i : UINT64_C(0));
+  }
+  return bb;
+}
 
 class Board {
   friend class BoardTest;
@@ -14,6 +64,7 @@ public:
   Board();
   static const int N_COLUMNS = 7;
   static const int N_ROWS = 6;
+  static const int COLUMN_BIT_OFFSET = 9;
   enum Player { P_EMPTY = 0, P_YELLOW = 1, P_RED = 2 };
   static const size_t N_VALID_BOARD_VALUES = 3; // P_EMPTY, P_YELLOW, P_RED
   using TBitBoard = uint64_t;
@@ -22,12 +73,27 @@ public:
   using THeightsArray = std::array<THeight, N_COLUMNS>;
   using TBoardArray = std::array<std::array<int32_t, N_ROWS>, N_COLUMNS>;
 
+private:
+  /* [ *,  *,  *,  *,  *,  *,  *]
+   * [ *,  *,  *,  *,  *,  *,  *]
+   * [ *,  *,  *,  *,  *,  *,  *]
+   * [59, 50, 41, 32, 23, 14,  5],
+   * [58, 49, 40, 31, 22, 13,  4],
+   * [57, 48, 39, 30, 21, 12,  3],
+   * [56, 47, 38, 29, 20, 11,  2],
+   * [55, 46, 37, 28, 19, 10,  1],
+   * [54, 45, 36, 27, 18,  9,  0]
+   */
+  static constexpr auto BOTTOM_ROW_BITS = {54, 45, 36, 27, 18, 9, 0};
+  static constexpr TBitBoard BB_BOTTOM_ROW = getMask(BOTTOM_ROW_BITS);
+  static constexpr auto TOP_ROW_BITS = {59, 50, 41, 32, 23, 14, 5};
+  static constexpr TBitBoard BB_TOP_ROW = getMask(TOP_ROW_BITS);
+  static constexpr TBitBoard BB_ILLEGAL = illegalBitMask();
+  static constexpr TBitBoard BB_ALL_LEGAL_TOKENS = ~BB_ILLEGAL;
+  static constexpr TBitBoard BB_EMPTY{UINT64_C(0)};
+
+public:
   bool setBoard(const TBoardArray &board);
-
-  static THeightsArray getColumnHeights(const TBitBoard &bbYellow,
-                                        const TBitBoard &bbRed);
-
-  static TBoardArray toArray(const TBitBoard &bbYellow, const TBitBoard &bbRed);
 
   TBoardArray toArray();
 
@@ -37,21 +103,92 @@ public:
 
   bool hasWin();
 
+  std::string toString() {
+    // TODO: Clean up. Messy. Magic numbers, etc...
+    std::stringstream ss;
+    ss << "\n";
+    auto arr = toArray();
+    for (int r = 5; r >= 0; r--) {
+      for (int c = 0; c < 7; c++) {
+        if (arr[c][r] == P_RED) {
+          ss << "o";
+        } else if (arr[c][r] == P_YELLOW) {
+          ss << "x";
+        } else {
+          ss << ".";
+        }
+      }
+      ss << "\n";
+    }
+    return ss.str();
+  }
+
+  inline TMovesCounter movesLeft() { return m_movesLeft; }
+
 private:
   /* Having a bitboard that contains all stones and another one representing the
    * current active player has the advantage that we do not have to do any
    * branching to figure out which player's turn it is. After each move we
    * simply apply an XOR-operation to switch players. */
-  TBitBoard m_bAll, m_bActive;
+  /* [ *,  *,  *,  *,  *,  *,  *]
+   * [ *,  *,  *,  *,  *,  *,  *]
+   * [ *,  *,  *,  *,  *,  *,  *]
+   * [59, 50, 41, 32, 23, 14,  5],
+   * [58, 49, 40, 31, 22, 13,  4],
+   * [57, 48, 39, 30, 21, 12,  3],
+   * [56, 47, 38, 29, 20, 11,  2],
+   * [55, 46, 37, 28, 19, 10,  1],
+   * [54, 45, 36, 27, 18,  9,  0]
+   */
+  TBitBoard m_bAll, m_bActive; // TODO: rename to m_bAllTokens, m_bActivePTokens
   TMovesCounter m_movesLeft;
 
-  // Private Functions
-private:
-  static inline TBitBoard getMask(int col_idx, int row_idx) {
-    const auto bit_idx = N_COLUMNS * N_ROWS - 1 - (col_idx * N_ROWS + row_idx);
-    return UINT64_C(1) << bit_idx;
+  static TBitBoard winningPositions(TBitBoard x);
+  bool isLegalMove(int column);
+
+  auto static inline constexpr getColumnMask(int column) {
+    assert(column >= 0 && column < N_COLUMNS);
+    return (UINT64_C(1) << (column * COLUMN_BIT_OFFSET + N_ROWS)) -
+           (UINT64_C(1) << (column * COLUMN_BIT_OFFSET));
   }
-  void playMoveFast(int column);
+
+  static inline constexpr uint64_t getMaskColRow(int column, int row) {
+    assert(column >= 0 && column < N_COLUMNS);
+    assert(row >= 0 && row < N_ROWS);
+    return UINT64_C(1) << (column * COLUMN_BIT_OFFSET + row);
+  }
+
+  static constexpr int popCountBoard(uint64_t x) {
+    int count = 0;
+    while (x) {
+      count += static_cast<int>(x & 1);
+      x >>= 1;
+    }
+    return count;
+  }
+
+  static inline constexpr Player opponent(Player p) {
+    return static_cast<Player>(3 - p);
+  }
+
+  void inline playMoveFast(int column) {
+    assert(column >= 0 && column < N_COLUMNS);
+    TBitBoard columnMask = getColumnMask(column);
+    assert(uint64_t_popcnt(columnMask) == N_ROWS);
+    auto mvMask = (m_bAll + BB_BOTTOM_ROW) & columnMask;
+    playMoveFastBB(mvMask);
+  }
+
+  void inline playMoveFastBB(TBitBoard mv) {
+    assert(mv != BB_EMPTY); // TODO: Only in Debug mode
+    assert((mv & BB_ILLEGAL) == BB_EMPTY);
+    m_bActive ^= m_bAll; // Already, switch player
+
+    // However, move is performed for current player (assuming, above switch is
+    // not yet performed)
+    m_bAll ^= mv; // bitwise xor and bitwise or are equivalent here
+    m_movesLeft--;
+  }
 };
 
 } // namespace BitBully
