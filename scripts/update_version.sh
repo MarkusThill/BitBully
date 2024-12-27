18af0512f55b5c3707869f98a46fd92d773416c0
MarkusThill <8896197+MarkusThill@users.noreply.github.com>
2024-12-27 17:04:10 +0100

    fix: fix version in script

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# https://commitizen-tools.github.io/commitizen/commands/bump/#pre_bump_hooks
+# Variable	Description
+# CZ_PRE_IS_INITIAL	True when this is the initial release, False otherwise
+# CZ_PRE_CURRENT_VERSION	Current version, before the bump
+# CZ_PRE_CURRENT_TAG_VERSION	Current version tag, before the bump
+# CZ_PRE_NEW_VERSION	New version, after the bump
+# CZ_PRE_NEW_TAG_VERSION	New version tag, after the bump
+# CZ_PRE_MESSAGE	Commit message of the bump
+# CZ_PRE_INCREMENT	Whether this is a MAJOR, MINOR or PATH release
+# CZ_PRE_CHANGELOG_FILE_NAME	Path to the changelog file, if available
+
 # Get the new version from Commitizen
-NEW_VERSION=$(cat version.txt)
+NEW_VERSION=$CZ_PRE_NEW_VERSION
 
 # Extract major, minor, and patch versions
 IFS='.' read -r MAJOR MINOR PATCH <<< "$NEW_VERSION"
