380871e20343bd5e91172530c40f74b842387fdb
Markus Thill <8896197+MarkusThill@users.noreply.github.com>
2025-08-13 23:45:03 +0200

    fix: make sure bitbully_core is created and added to wheel under windows

--- setup.py
+++ setup.py
@@ -4,12 +4,21 @@ This module defines custom setuptools extension and build classes to integrate
 CMake into the Python packaging and distribution workflow.
 """
 
+import os
 import subprocess
 import sys
 from pathlib import Path
 
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
+
+# Convert distutils Windows platform specifiers to CMake -A arguments
+PLAT_TO_CMAKE = {
+    "win32": "Win32",
+    "win-amd64": "x64",
+    "win-arm32": "ARM",
+    "win-arm64": "ARM64",
+}
 
 
 class CMakeBuildExtension(build_ext):
@@ -33,9 +42,44 @@ class CMakeBuildExtension(build_ext):
         cfg = "Debug" if self.debug else "Release"
         cmake_args = [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
+            f"-DCMAKE_RUNTIME_OUTPUT_DIRECTORY={extdir}",
+            f"-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY={extdir}",
             f"-DPYTHON_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE={cfg}",
         ]
+
+        build_args = []
+
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+        # TODO: Windows specific:
+        if self.compiler.compiler_type != "msvc":
+            pass
+            # Not sure if we have to do something here. Check:
+            # https://github.com/pybind/cmake_example/blob/master/setup.py
+        else:
+            # Single config generators are handled "normally"
+            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+
+            # CMake allows an arch-in-generator style for backward compatibility
+            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+
+            # Specify the arch if using MSVC generator, but only if it doesn't
+            # contain a backward-compatibility arch spec already in the
+            # generator name.
+            if not single_config and not contains_arch:
+                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
+
+            # Multi-config generators have a different way to specify configs
+            if not single_config:
+                # Config-specific dirs (critical for MSVC multi-config)
+                cmake_args += [
+                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}",
+                    f"-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}",
+                    f"-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}",
+                ]
+                build_args += ["--config", cfg]
 
         # Create the build directory
         if not Path(self.build_temp).exists():
@@ -47,7 +91,8 @@ class CMakeBuildExtension(build_ext):
         )
         # Run the build
         subprocess.check_call(
-            ["cmake", "--build", ".", "--target", "bitbully_core"], cwd=self.build_temp
+            ["cmake", "--build", ".", "--target", "bitbully_core", *build_args],
+            cwd=self.build_temp,
         )
 
 
