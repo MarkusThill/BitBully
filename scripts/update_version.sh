804d28b5e1728563345d098e637070533dde797f
Markus Thill <8896197+MarkusThill@users.noreply.github.com>
2025-09-13 13:08:30 +0200

    fix: allow fast builds with prereleases 2

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -20,9 +20,9 @@ IFS='.' read -r MAJOR MINOR PATCH <<< "$NEW_VERSION"
 # Update version.h
 sed -i.bak \
     -e "s/^#define PROJECT_VERSION \".*\"/#define PROJECT_VERSION \"$NEW_VERSION\"/" \
-    -e "s/^#define PROJECT_MAJOR_VERSION .*/#define PROJECT_MAJOR_VERSION $MAJOR/" \
-    -e "s/^#define PROJECT_MINOR_VERSION .*/#define PROJECT_MINOR_VERSION $MINOR/" \
-    -e "s/^#define PROJECT_PATCH_VERSION .*/#define PROJECT_PATCH_VERSION $PATCH/" \
+    -e "s/^#define PROJECT_MAJOR_VERSION .*/#define PROJECT_MAJOR_VERSION \"$MAJOR\"/" \
+    -e "s/^#define PROJECT_MINOR_VERSION .*/#define PROJECT_MINOR_VERSION \"$MINOR\"/" \
+    -e "s/^#define PROJECT_PATCH_VERSION .*/#define PROJECT_PATCH_VERSION \"$PATCH\"/" \
     src/version.h
 
 # Remove the backup file created by sed (on macOS)
