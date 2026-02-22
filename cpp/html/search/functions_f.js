e3c5b77e44b7557153a2b3ab9e706aaf859bf12e
MarkusThill <MarkusThill@users.noreply.github.com>
2026-02-22 19:56:27 +0000

    deploy: 2b8db9492542289f115da542ad9af5cb505fccdb

--- cpp/html/search/functions_f.js
+++ cpp/html/search/functions_f.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['score_5fall_5fmoves_0',['score_all_moves',['../d0/d3b/classbitbully_1_1agent__interface_1_1Connect4Agent.html#ac2b74bd9092a56a8e08d5c0726c9f3c3',1,'bitbully.agent_interface.Connect4Agent.score_all_moves()'],['../dc/d09/classbitbully_1_1solver_1_1BitBully.html#a5e903e22e59d69e9067fcc6bd781eed5',1,'bitbully.solver.BitBully.score_all_moves()']]],
-  ['score_5fmove_1',['score_move',['../d0/d3b/classbitbully_1_1agent__interface_1_1Connect4Agent.html#aae72966d4929d407416107d22e988dec',1,'bitbully.agent_interface.Connect4Agent.score_move()'],['../dc/d09/classbitbully_1_1solver_1_1BitBully.html#a0a7a5b79197e87b2e47054834235cd27',1,'bitbully.solver.BitBully.score_move()']]]
+  ['score_5fmove_1',['score_move',['../d0/d3b/classbitbully_1_1agent__interface_1_1Connect4Agent.html#aae72966d4929d407416107d22e988dec',1,'bitbully.agent_interface.Connect4Agent.score_move()'],['../dc/d09/classbitbully_1_1solver_1_1BitBully.html#a0a7a5b79197e87b2e47054834235cd27',1,'bitbully.solver.BitBully.score_move(self, Board board, int column, int first_guess=0, *, int|None max_depth=None)']]],
+  ['score_5fto_5fmoves_5fleft_2',['score_to_moves_left',['../dc/d09/classbitbully_1_1solver_1_1BitBully.html#a1f8d0858dba82ddc4dd8bdb14ab21337',1,'bitbully::solver::BitBully']]],
+  ['scoretomovesleft_3',['scoreToMovesLeft',['../d9/d00/classBitBully_1_1BitBully.html#a5c568bf905c59bfe05913b25052cf44c',1,'BitBully::BitBully']]]
 ];
